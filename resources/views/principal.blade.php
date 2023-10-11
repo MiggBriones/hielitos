db05db0daf84ab8da5cd36112ff7e6a9fef2e5cf
Miguel Briones <migg.briones@gmail.com>
2023-10-11 01:06:53 -0600

    Agregar vistas, rutas y plantilla para no repetir código del menú principal.

---
+++ resources/views/principal.blade.php
@@ -0,0 +1,13 @@
+<!-- 
+    Se posiciona por defecto en la carpeta views 
+-->
+
+@extends('layouts.app')
+
+@section('titulo')
+    Página principal
+@endsection
+
+@section('contenido')
+    Contenido de esta página
+@endsection
---
+++ resources/views/tienda.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('titulo')
+    Tienda
+@endsection
+
+@section('contenido')
+    Sobre la tienda
+@endsection
