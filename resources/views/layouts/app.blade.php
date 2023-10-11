128525ff47b8bf248095e149f4e76896161af548
Miguel Briones <migg.briones@gmail.com>
2023-10-11 01:15:24 -0600

    Agregar vista de registro de cuenta y cambiar estilo de footer. Se excluye la carpetade imagenes en public para evitar versionar las imagenes.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,7 +28,7 @@
             </h2>
             @yield('contenido')
         </main>
-        <footer class="text-center p-5 text-gray-500 font-bold uppercase">
+        <footer class="mt-10 text-center p-5 text-gray-500 font-bold uppercase">
             <h1>Devstagram - Todos los derechos reservados {{ now()->year }} </h1>
         </footer>
     </body>
