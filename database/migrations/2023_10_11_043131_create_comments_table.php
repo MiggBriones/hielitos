e36a83956cd694e5c7d2eb09a9e216a1df446281
Miguel Briones <migg.briones@gmail.com>
2023-10-11 01:40:59 -0600

    Agregar migraciones de modelo de ER.

---
+++ database/migrations/2023_10_11_043131_create_comments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->integer('id', true);
+            $table->text('comment');
+            $table->integer('id_maintenance')->index('fk_comments_maintenance1_idx');
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+};
