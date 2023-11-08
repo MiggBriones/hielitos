2645159f857fa2c7620d80ca316b182d85c3143a
Miguel Briones <migg.briones@gmail.com>
2023-11-08 00:02:28 -0600

    Productos - Se agrega boton para editar el registro.

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -26,6 +26,7 @@
                     <th class="px-6 py-4">Color</th>
                     <th class="px-6 py-4">Motor</th>
                     <th class="px-6 py-4">Gas</th>
+                    <th class="px-6 py-4">Editar</th>
                 </tr>
             </thead>
             <tbody>
@@ -40,6 +41,13 @@
                         <td class="px-6 py-3">{{$product->getProductWithColor->description}}</td>
                         <td class="px-6 py-3">{{$product->getProductWithEngineSize->size}}</td>
                         <td class="px-6 py-3">{{$product->getProductWithGasType->description}}</td>
+                        <td class="px-6 py-3">
+                            <a href="#" >
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                </svg>
+                            </a>
+                        </td>
                     </tr>
                 @endforeach
             </tbody>
