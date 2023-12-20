a6bdecb6646596fa36919abc8cbcca90c1cca2cf
Miguel Briones <migg.briones@gmail.com>
2023-12-20 00:59:54 -0600

    Mantenimiento - Actualizar el estatus en la bd.

--- resources/views/maintenance/edit.blade.php
+++ resources/views/maintenance/edit.blade.php
@@ -40,11 +40,12 @@
                     <textarea
                         id="observacion"
                         name="observacion"
-                        rows="4" 
+                        rows="12" 
                         class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border
                             border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700
                             dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500
                             dark:focus:border-blue-500"
+                        readonly="readonly"
                     >
                     {{ $maintenance->observation }}
                     </textarea>
