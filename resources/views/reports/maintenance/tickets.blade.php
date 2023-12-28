d9a56cc2ad2f90bc0d35605a121203ee2dc0b0ce
Miguel Briones <migg.briones@gmail.com>
2023-12-28 14:33:55 -0600

    Mantenimiento - Agregar tables y elementos del reporte.

--- resources/views/reports/maintenance/tickets.blade.php
+++ resources/views/reports/maintenance/tickets.blade.php
@@ -5,48 +5,122 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         @stack('styles')
         <title>Mantenimientos</title>
-        @vite('resources/css/app.css')
-        @vite('resources/js/app.js')
+        <style>
+           
+           table {
+                margin: 1em;
+                padding: 0.2em;
+            }
+
+            .encabezado {
+                text-align: center;
+                background-color: #F58585;
+            }
+
+            .falla {
+                text-align: center;
+                background-color: #B2F6D7;
+            }
+
+            .mantenimiento {
+                text-align: center;
+                background-color: #F58585;
+            }
+
+            .funcionamiento {
+                text-align: center;
+                background-color: #F58585;
+            }
+            
+            tr td:first-child {
+                width: 100%;
+                background-color: #F1EDA5;
+            }
+
+        </style>
     </head>
-    <body class="bg-gray-100">
-        <header class="p-5 border-b bg-white shadow">
-            <h1 class="text-3xl font-black">
-                Listado de mantenimientos
+    <body class="">
+        <header class="encabezado">
+            <h1 class="">
+                HIELO Y FRIGORIFICO DE COATZACOALCOS S.A.
             </h1>
+            <h3> REPORTE DE SERVICIO</h3>
         </header>
-        <main class="container mx-auto mt-10">
-            <h2 class="font-black text-center text-3xl mb-10">
+        <main class="">
+            <h2 class="">
                 @yield('titulo')
             </h2>
 
-            <div class="mt-10 text-center p-5 text-gray-500 font-bold uppercase">
-                <div class="grid place-items-center">    
-                    <table class="text-sm text-left text-gray-500 dark:text-gray-400">                
-                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                            <tr>
-                                <th class="px-6 py-4">Id</th>
-                                <th class="px-6 py-4"># serie</th>
-                                <th class="px-6 py-4">Producto</th>
-                                <th class="px-6 py-4">Marca</th>
-                                <th class="px-6 py-4">Fecha</th>
-                            </tr>
-                        </thead>
-                
-                        <tbody>
-                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                <td class="px-6 py-3">{{$maintenance->id}}</td>
-                                <td class="px-6 py-3">{{$product->serial_number}}</td>
-                                <td class="px-6 py-3">{{$product->description}}</td>
-                                <td class="px-6 py-3">{{$product->getProductWithBrand->description}}</td>
-                                <td class="px-6 py-3">{{ $maintenance->created_at->format('Y-m-d') }}</td>
-                            </tr>
-                        </tbody>
-                
-                    </table>
-                </div>
+            <div class="falla">
+                <table class="">                           
+                    <tbody>
+                        <tr class="">
+                            <td class=""></td>
+                            <td class="">REPORTE DE FALLA DEL CONSERVADOR</td>
+                            <td class=""></td>
+                        </tr>
+
+                        <tr class="">
+                            <td class="">Fecha</td>
+                            <td class="">Hora</td>
+                            <td class="">Cliente</td>
+                        </tr>
+
+                        <tr class="">
+                            <td class="">Reporta</td>
+                            <td class="">Capacidad de conservador</td>
+                        </tr>
+
+                        <tr class="">
+                            <td class="">Nombre y firma de quien reporta</td>
+                            <td class="">Nombre y firma de quien recibe reporte</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="mantenimiento">
+                <table class="">                
+                    <tbody>
+                        <tr class="">
+                            <td class="">Id</td>
+                            <td class=""># serie</td>
+                            <td class="">Producto</td>
+                            <td class="">Marca</td>
+                            <td class="">Fecha</td>
+                        </tr>
+            
+                        <tr class="">
+                            <td class="">{{$maintenance->id}}</td>
+                            <td class="">{{$product->serial_number}}</td>
+                            <td class="">{{$product->description}}</td>
+                            <td class="">{{$product->getProductWithBrand->description}}</td>
+                            <td class="">{{ $maintenance->created_at->format('Y-m-d') }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+
+            <div class="funcionamiento">
+                <table class="">
+                    <tr class="">
+                        <td class="">FUNCIONAMIENTO</td>
+                    </tr>
+            
+                    <tbody>
+                        <tr class="">
+                            <td class="">{{$maintenance->id}}</td>
+                            <td class="">{{$product->serial_number}}</td>
+                            <td class="">{{$product->description}}</td>
+                            <td class="">{{$product->getProductWithBrand->description}}</td>
+                            <td class="">{{ $maintenance->created_at->format('Y-m-d') }}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </main>
-        <footer class="mt-10 text-center p-5 text-gray-500 font-bold uppercase">
+        <footer class="">
             <h1>Hielitos - Todos los derechos reservados {{ now()->year }} </h1>
         </footer>
     </body>
